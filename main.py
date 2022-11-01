0744a775a599268dccdeb4bedcbee74100b0938c
40253397 <adungo01@qub.ac.uk>
2022-11-01 14:47:37 +0000

    Created Authentication endpoints for email/password

--- main.py
+++ main.py
@@ -24,9 +24,47 @@ app.add_middleware(
 
 # signup endpoint
 
+
+@app.post("/signup", include_in_schema=False)
+async def signup(request: Request):
+    req = await request.json()
+    email = req['email']
+    password = req['password']
+    if email is None or password is None:
+        return HTTPException(detail={'message': 'Error! Missing Email or Password'}, status_code=400)
+    try:
+        user = auth.create_user(
+            email=email,
+            password=password
+        )
+        return JSONResponse(content={'message': f'Successfully created user {user.uid}'}, status_code=200)
+    except:
+        return HTTPException(detail={'message': 'Error Creating User'}, status_code=400)
+
+
 # login endpoint
+@app.post("/login", include_in_schema=False)
+async def login(request: Request):
+    req_json = await request.json()
+    email = req_json['email']
+    password = req_json['password']
+    try:
+        user = pb.auth().sign_in_with_email_and_password(email, password)
+        jwt = user['idToken']
+        return JSONResponse(content={'token': jwt}, status_code=200)
+    except:
+        return HTTPException(detail={'message': 'There was an error logging in'}, status_code=400)
+
 
 # ping endpoint
+@app.post("/ping", include_in_schema=False)
+async def validate(request: Request):
+    headers = request.headers
+    jwt = headers.get('authorization')
+    print(f"jwt:{jwt}")
+    user = auth.verify_id_token(jwt)
+    return user["uid"]
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app")

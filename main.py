dcda22053c6dde166b4bf0e4e1994e2b561dcf21
40253397 <adungo01@qub.ac.uk>
2022-11-01 14:46:01 +0000

    Set up pyrebase firebase admin and fastapi

---
+++ main.py
@@ -0,0 +1,32 @@
+import uvicorn
+import firebase_admin
+import pyrebase
+import json
+
+from firebase_admin import credentials, auth
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
+
+cred = credentials.Certificate('swapit-5eb81_service_account_keys.json')
+firebase = firebase_admin.initialize_app(cred)
+pb = pyrebase.initialize_app(json.load(open('firebase_config.json')))
+app = FastAPI()
+allow_all = ['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allow_all,
+    allow_credentials=True,
+    allow_methods=allow_all,
+    allow_headers=allow_all
+)
+
+# signup endpoint
+
+# login endpoint
+
+# ping endpoint
+
+if __name__ == "__main__":
+    uvicorn.run("main:app")

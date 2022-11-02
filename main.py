25a504fb3e8175c819040800449280e5d1c30c9c
40253397 <adungo01@qub.ac.uk>
2022-11-02 13:26:55 +0000

    Moved signup and login service to client-side for ease of use

--- main.py
+++ main.py
@@ -31,45 +31,9 @@ app.add_middleware(
     allow_headers=allow_all
 )
 
-# signup endpoint
-
-
-@app.post("/signup", include_in_schema=False)
-async def signup(request: Request):
-    req = await request.json()
-    email = req['email']
-    password = req['password']
-    if email is None or password is None:
-        logger.warning("Request is missing email or password")
-        return HTTPException(detail={'message': 'Error! Missing Email or Password'}, status_code=400)
-    try:
-        auth.create_user(
-            email=email,
-            password=password
-        )
-        return JSONResponse(content={'message': f'User successfully created.'}, status_code=200)
-    except Exception as err:
-        logger.error(str(err))
-        return HTTPException(detail={'message': str(err)}, status_code=400)
-
-
-# login endpoint
-@app.post("/login", include_in_schema=False)
-async def login(request: Request):
-    req_json = await request.json()
-    email = req_json['email']
-    password = req_json['password']
-    try:
-        user = pb.auth().sign_in_with_email_and_password(email, password)
-        jwt = user['idToken']
-        return JSONResponse(content={'token': jwt}, status_code=200)
-    except Exception as err:
-        res = FormatFireBaseError(err)
-        logger.error(res["message"])
-        return HTTPException(detail={'message': res["message"]}, status_code=res["code"])
-
-
 # ping endpoint
+
+
 @app.post("/ping", include_in_schema=False)
 async def validate(request: Request):
     headers = request.headers

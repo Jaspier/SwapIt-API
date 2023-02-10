36d90108b5c6b1d8b527556758c398e2610832e9
40253397 <adungo01@qub.ac.uk>
2023-02-10 17:27:14 +0000

    Removed unused FormatFireBaseError utility function

--- utilities.py
+++ utilities.py
@@ -4,13 +4,6 @@ import datetime
 from models import UserObject
 
 
-def FormatFireBaseError(res: str):
-    stri = str(res).split("\n", 1)[1]
-    formatted = re.sub(r'^.*?"error"', '{\n"error"', stri)
-    res = json.loads(formatted)
-    return res["error"]
-
-
 def FormatFireBaseDoc(doc: object):
     for key, value in doc.items():
         if key == "timestamp":
